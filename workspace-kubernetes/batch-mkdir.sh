7b0fcc46d76be1da8a605e7b9eecad49068f4053
humstarman <lueyoung7@gmail.com>
2018-02-10 12:38:20 +0800

    update installation of k8s

---
+++ workspace-kubernetes/batch-mkdir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ansible all -m shell -a "mkdir -p /var/env && mkdir -p /etc/kubernetes/ssl && mkdir -p /etc/flanneld/ssl && mkdir -p /var/lib/kubelet && mkdir -p /var/lib/kube-proxy && mkdir -p /etc/kubernetes/manifests"
+
+ansible master -m shell -a "mkdir -p /etc/etcd/ssl && mkdir -p /var/lib/etcd"

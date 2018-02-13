263ac4b1c69145244a6919b615b60c4467a8bc5a
humstarman <lueyoung7@gmail.com>
2018-02-13 15:18:44 +0800

    update harbor dir

--- workspace-kubernetes/batch-mkdir.sh
+++ workspace-kubernetes/batch-mkdir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ansible all -m shell -a "mkdir -p /var/env && mkdir -p /etc/kubernetes/ssl && mkdir -p /etc/flanneld/ssl && mkdir -p /var/lib/kubelet && mkdir -p /var/lib/kube-proxy && mkdir -p /etc/kubernetes/manifests && mkdir -p /etc/calico"
+ansible all -m shell -a "mkdir -p /var/env && mkdir -p /etc/kubernetes/ssl && mkdir -p /etc/flanneld/ssl && mkdir -p /var/lib/kubelet && mkdir -p /var/lib/kube-proxy && mkdir -p /etc/kubernetes/manifests && mkdir -p /etc/calico && mkdir -p /etc/harbor/ssl"
 
 ansible master -m shell -a "mkdir -p /etc/etcd/ssl && mkdir -p /var/lib/etcd"

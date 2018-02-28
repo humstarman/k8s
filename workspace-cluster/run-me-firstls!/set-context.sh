3e16d96eeda7c80d70fa03b2b5eb6d30998ab0a8
humstarman <lueyoung7@gmail.com>
2018-02-28 09:40:37 +0800

    patameterize

--- workspace-cluster/run-me-firstls!/set-context.sh
+++ workspace-cluster/run-me-firstls!/set-context.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -e
+
+CONTEXT=dc
+NAMESPACEi=datacentre
+
+kubectl config use-context kubernetes
 CURRENT_CONTEXT=$(kubectl config view -o jsonpath='{.current-context}')
 USER_NAME=$(kubectl config view -o jsonpath='{.contexts[?(@.name == "'"${CURRENT_CONTEXT}"'")].context.user}')
 CLUSTER_NAME=$(kubectl config view -o jsonpath='{.contexts[?(@.name == "'"${CURRENT_CONTEXT}"'")].context.cluster}')
-kubectl config set-context dc --namespace=datacentre --cluster=${CLUSTER_NAME} --user=${USER_NAME}
-kubectl config use-context dc 
+kubectl config set-context $CONTEXT --namespace=$NAMESPACE --cluster=${CLUSTER_NAME} --user=${USER_NAME}
+kubectl config use-context $CONTEXT
+

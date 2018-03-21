cba75f0000ca89cc31f9a3942e5c8a5e0a284b92
humstarman <lueyoung7@gmail.com>
2018-03-21 15:40:49 +0800

    update: make dir of /etc/etcd/ssl to all

--- workspace-kubernetes/batch-mkdir.sh
+++ workspace-kubernetes/batch-mkdir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ansible all -m shell -a "mkdir -p /var/env && mkdir -p /etc/kubernetes/ssl && mkdir -p /etc/flanneld/ssl && mkdir -p /var/lib/kubelet && mkdir -p /var/lib/kube-proxy && mkdir -p /etc/kubernetes/manifests && mkdir -p /etc/calico && mkdir -p /etc/harbor/ssl"
+ansible all -m shell -a "mkdir -p /var/env && mkdir -p /etc/kubernetes/ssl && mkdir -p /etc/flanneld/ssl && mkdir -p /var/lib/kubelet && mkdir -p /var/lib/kube-proxy && mkdir -p /etc/kubernetes/manifests && mkdir -p /etc/calico && mkdir -p /etc/harbor/ssl && mkdir -p /etc/etcd/ssl"
 
-ansible master -m shell -a "mkdir -p /etc/etcd/ssl && mkdir -p /var/lib/etcd"
+ansible master -m shell -a "mkdir -p /var/lib/etcd"
